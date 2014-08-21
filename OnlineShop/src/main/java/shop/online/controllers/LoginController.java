5d05260f7bbc80cd79521db8fc7793e1a479864e
Ann Kornelik <annnet93@gmail.com>
2014-08-21 23:26:11 +0300

    Online Shop project

---
+++ OnlineShop/src/main/java/shop/online/controllers/LoginController.java
@@ -0,0 +1,39 @@
+package shop.online.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import shop.online.db.Account;
+import shop.online.services.AccountService;
+
+/**
+ * Created by ann on 8/20/14.
+ */
+
+@Controller
+@RequestMapping("")
+public class LoginController {
+
+    @Autowired
+    AccountService accountService;
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String getLogin(Model model) {
+        model.addAttribute("loginAttribute", new Account());
+        return "login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(@ModelAttribute("loginAttribute") Account account) {
+        try {
+            accountService.getAccount(new Account(), account.getUsername(), account.getPassword());
+            return "redirect:/secured/home";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "registration";
+        }
+    }
+}
---
+++ OnlineShop/src/main/java/shop/online/services/ManagerDaoImpl.java
@@ -0,0 +1,54 @@
+package shop.online.services;
+
+import org.hibernate.Criteria;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hibernate.criterion.Restrictions.like;
+
+/**
+ * Created by ann on 8/19/14.
+ */
+@Repository("managerDAO")
+@Transactional
+public class ManagerDaoImpl<T> implements IManagerDao<T> {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    @Override
+    public void save(T t) {
+        getSessionFactory().getCurrentSession().save(t);
+    }
+
+    @Override
+    public void update(T t) {
+        getSessionFactory().getCurrentSession().update(t);
+    }
+
+    @Override
+    public T getById(T t, Integer id) {
+        return (T) getSessionFactory().getCurrentSession().get(t.getClass(), id);
+    }
+
+    @Override
+    public T getAccount(T t, String username, String password) {
+        Criteria criteria = getSessionFactory().getCurrentSession()
+                .createCriteria(t.getClass()).add((like("username", username)))
+                .add((like("password", password)));
+        return (T) criteria.uniqueResult();
+    }
+
+    @Override
+    public T getAccountByName(T t, String username) {
+        Criteria criteria = getSessionFactory().getCurrentSession()
+                .createCriteria(t.getClass()).add(like("username", username));
+        return (T) criteria.uniqueResult();
+    }
+}
